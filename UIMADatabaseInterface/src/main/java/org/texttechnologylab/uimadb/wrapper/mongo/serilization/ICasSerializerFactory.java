b6e3ef8b1951d5438c413fe851b5ec0835be1f8e
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2019-11-28 17:36:42 +0100

    move

--- UIMADatabaseInterface/src/main/java/org/texttechnologylab/uimadb/wrapper/mongo/serilization/ICasSerializerFactory.java
+++
@@ -1,8 +0,0 @@
-package org.texttechnologylab.uimadb.wrapper.mongo.serilization;
-
-
-import org.texttechnologylab.uimadb.wrapper.mongo.serilization.exceptions.SerializerInitializationException;
-
-public interface ICasSerializerFactory {
-	ICasSerializer createSerializer() throws SerializerInitializationException;
-}
